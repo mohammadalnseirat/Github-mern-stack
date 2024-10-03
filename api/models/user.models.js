01bb0df3a92edb809c612ac556113c6a8495ffcf
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-10-03 23:34:14 +0400

    Create User Model

--- api/models/user.models.js
+++ api/models/user.models.js
@@ -1,6 +1,45 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    profileUrl: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: {
+      type: String,
+    },
+    likedProfiles: {
+      type: [String],
+      default: [],
+    },
+    likedBy: [
+      {
+        username: {
+          type: String,
+          required: true,
+        },
+        avatarUrl: {
+          type: String,
+        },
+        likedDate: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 // create the model:
 const User = mongoose.model("User", userSchema);
